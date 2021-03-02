049f0a46c073e7003bf1aac279bcc392582981c9
vssaAnjos <anjnessa9@gmail.com>
2021-03-02 09:32:06 -0300

    adicionando body-parser para utilizar no método post

--- config/customExpress.js
+++ config/customExpress.js
@@ -4,10 +4,16 @@
 const express = require('express');
 const consign = require('consign');
 const app= express();
+const bodyParser = require('body-parser');
 
 module.exports = () => {
     const app= express();
 // configacao da aplicacao
+
+    app.use(bodyParser.urlencoded({extended:true}))
+    app.use(bodyParser.json())
+    // app.user = adc bibliotecas para uso
+
     consign() // pesquisar o consign
         .include('controllers')
         .into(app)
