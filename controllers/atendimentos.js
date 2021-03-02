14e81f23fac149344f92b702765f364bdb288663
vssaAnjos <anjnessa9@gmail.com>
2021-03-02 09:19:20 -0300

    adicionando método post

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -7,5 +7,10 @@ module.exports = app => {
 // Res = response
 // Response no get
 // app.get('/',(req,res) => res.send('Servidor funcionando...!'))
-    app.get('/atendimentos',(req,res) => res.send('Vamos atender mesmo..!'))
+    app.get('/atendimentos',(req,res) => res.send('GET: Vamos atender mesmo..!'))
+
+    app.post('/atendimentos',(req,res) => {
+        console.log(req.body)
+        res.send('POST: Você está na rota de atendimentos via POST')
+    })
 }
