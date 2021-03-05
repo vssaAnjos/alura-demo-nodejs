07cca215d66c042b32f30ec1209cbf56a44eecca
vssaAnjos <anjnessa9@gmail.com>
2021-03-05 09:37:45 -0300

    conectando com banco de dados - mysql

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // modulos
 
 const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
+ 
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro)
+    } else {
+        console.log("Conectado com sucesso!")
+        const app = customExpress()
+        app.listen(3000,() => console.log('servidor ouvindo na porta 3000!'));
+    }
+})
 
-const app = customExpress()
-app.listen(3000,() => console.log('servidor ouvindo na porta 3000!'));
 
 
 
