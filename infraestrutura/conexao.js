3bc7671d33b3fcebb572895019b16ae9a3155b2d
vssaAnjos <anjnessa9@gmail.com>
2021-04-12 09:44:15 -0300

    update

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -4,7 +4,7 @@ const conexao = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'eclipse90',
+    password: '',
     database: 'agenda_petshop'
 })
 
