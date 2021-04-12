846b1f7a2a88da63b8ca849c952ed8dbcf321886
vssaAnjos <anjnessa9@gmail.com>
2021-04-12 09:42:04 -0300

    adicionando campos de data no atendimento

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,9 +1,15 @@
+const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
+
 class Atendimento{
     adicionaAtendimento(atendimento){
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
+        const dataAtendimento = moment (atendimento.dataAtendimento,'DD-MM-YYYY').format('YYYY-MM-DD HH:mm:ss')
+        const atendimentosDatado = {...atendimento, dataCriacao,dataAtendimento}
+
         const sql = 'INSERT INTO Atendimentos SET ?'
 
-        conexao.query(sql,atendimento, (erro, resultados) => {
+        conexao.query(sql,atendimentosDatado  , (erro, resultados) => {
             if(erro) {
                 console.log(erro)
             } else {
---
+++ querysDatabase.sql
@@ -0,0 +1,1 @@
+alter table agenda_petshop.Atendimentos add `dataAtendimento` datetime not null DEFAULT '1970-01-01 00:00:00', add `dataCriacao` datetime not null DEFAULT '1970-01-01 00:00:00';    
