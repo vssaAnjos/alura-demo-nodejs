5ae3e9ac8b9047c58f13dd8964a5845b3aaf546f
vssaAnjos <anjnessa9@gmail.com>
2021-04-13 09:40:40 -0300

    aplicando validações da entrada da requisição

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -7,19 +7,51 @@ class Atendimento{
         const dataAtendimento = moment (atendimento.dataAtendimento,'DD-MM-YYYY').format('YYYY-MM-DD HH:mm:ss')
         const atendimentosDatado = {...atendimento, dataCriacao,dataAtendimento}
 
-        const sql = 'INSERT INTO Atendimentos SET ?'
+        const dataAtendimentoEhValida = moment(dataAtendimento).isSameOrAfter(dataCriacao )
+        const clienteEhValido = atendimento.cliente.lenght >=5
+        console.log(atendimento.cliente.lenght)
+        console.log(clienteEhValido)
 
-        conexao.query(sql,atendimentosDatado  , (erro, resultados) => {
-            if(erro) {
-                res.status(400).json(erro)
-                console.log(erro)
-                
-            } else {
-                res.status(201).json(resultados)
-                console.log(resultados)
+        const validacoes = [
+            {
+                nome : 'dataAtendimento',
+                valido: dataAtendimentoEhValida,
+                mensagem : 'Data de Atendimento deve ser maior ou igual a data atual'
+            },
+            {
+                nome : 'cliente',
+                valido : clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos 5 caracteres'
             }
 
-        })
+        ]
+
+        const erros = validacoes.filter(campo=> !campo.valido)
+        console.log (erros)
+        const existemErros = erros.length
+        console.log (existemErros)
+
+        
+        if (existemErros) {
+            res.status(400).json(erros)
+        } else {
+
+            const sql = 'INSERT INTO Atendimentos SET ?'
+
+            conexao.query(sql,atendimentosDatado  , (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                    console.log(erro)
+                    
+                } else {
+                    res.status(201).json(resultados)
+                    console.log(resultados)
+                }
+    
+            })
+
+        }
+      
     }
 }
 
